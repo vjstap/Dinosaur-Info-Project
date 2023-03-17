eebe75d567736a772dbfcaba483a9ba6934139d6
vjstap <vincentjstapley@gmail.com>
2023-03-16 23:29:42 -0600

    Added Iframes
    Toggleable button for Wikipedia

---
+++ script.js
@@ -0,0 +1,12 @@
+let toggle = button => {
+    let element = document.getElementById("wikipediaiframe");
+
+    if (element.hidden) {
+      element.hidden = false;
+      button.innerText = "Hide Wikipedia";
+    } else {
+      element.hidden = true;
+      button.innerText = "Show Wikipedia";
+    }
+  }
+  
