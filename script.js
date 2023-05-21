92ff29b1ea054ac80500d310c334b31e8ec43455
vjstap <vincentjstapley@gmail.com>
2023-05-21 03:21:25 -0600

    surely i did any of this correctly
    clueless

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
 let toggle = button => {
-    let element = document.querySelector('.wikipediaiframe');
+  let element = document.querySelector('.wikipediaiframe');
 
-    if (element.hidden) {
-      element.hidden = false;
-      button.innerText = "Hide Wikipedia";
-    } else {
-      element.hidden = true;
-      button.innerText = "Show Wikipedia";
-    }
+  if (element.hidden) {
+    element.hidden = false;
+    button.innerText = "Hide Wikipedia";
+  } else {
+    element.hidden = true;
+    button.innerText = "Show Wikipedia";
   }
+}
 var modal = document.getElementById("myModal");
-var img = document.getElementById("myImg");
+var imgs = document.querySelectorAll(".myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-  
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+
+imgs.forEach(function(img) {
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+});
+
 window.onclick = function(event) {
   if (event.target == modal) {
-     modal.style.display = "none";
-    }
+    modal.style.display = "none";
+  }
 }
 
 let links = document.querySelectorAll(".sidebar a");
 
 links.forEach(link => {
-    let img = link.querySelector("img");
-    if (img) {
-        let originalSrc = img.src;
-        let hoverSrc = img.dataset.hoverSrc;
+  let img = link.querySelector("img");
+  if (img) {
+    let originalSrc = img.src;
+    let hoverSrc = img.dataset.hoverSrc;
 
-        link.addEventListener("mouseenter", function() {
-            img.src = hoverSrc;
-        });
+    link.addEventListener("mouseenter", function() {
+      img.src = hoverSrc;
+    });
 
-        link.addEventListener("mouseleave", function() {
-            img.src = originalSrc;
-        });
-    }
+    link.addEventListener("mouseleave", function() {
+      img.src = originalSrc;
+    });
+  }
 });
